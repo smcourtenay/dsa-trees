969d61e783c671d02c40d413c2c98649c3a38900
Sam Courtenay <sam.m.courtenay@gmail.com>
2022-04-13 11:15:20 -0700

    completed first 3 problems for trees.js

--- tree.js
+++ tree.js
@@ -15,20 +15,66 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
+
+    if(this.root === null) return 0;
+
+    let visitingStack = [this.root];
+    let sum = 0;
+
+    while (visitingStack.length !== 0){
+      let current = visitingStack.pop();
+      
+      for(let child of current.children){
+        visitingStack.push(child);
+      }
+      // console.log(current.val)
+      sum += current.val;
+    }
     
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    if(this.root === null) return 0;
 
+    let visitingStack = [this.root];
+    let count = 0;
+
+    while (visitingStack.length !== 0){
+      let current = visitingStack.pop();
+      
+      for(let child of current.children){
+        visitingStack.push(child);
+      }
+      // console.log(current.val)
+      if(current.val%2 === 0) count ++;
+    }
+    
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if(this.root === null) return 0;
 
+    let visitingStack = [this.root];
+    let count = 0;
+
+    while (visitingStack.length !== 0){
+      let current = visitingStack.pop();
+      
+      for(let child of current.children){
+        visitingStack.push(child);
+      }
+      // console.log(current.val)
+      if(current.val > lowerBound) count ++;
+    }
+    
+    return count;
   }
 }
 
